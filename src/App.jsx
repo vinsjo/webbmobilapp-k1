2f554ff04c73769f25d79b88f9d3c31bfcc35a83
Vincent Sjögren <vincentsjogren@gmail.com>
2022-10-18 14:48:08 +0200

    fixed error in @vinsjo/use-axios and fixed TimeTrackerProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from 'react';
+import { useTimeTracker } from './context/TimeTracker';
+
 const App = () => {
+    const { projects, tasks, timelogs } = useTimeTracker();
+    useEffect(() => {
+        console.log('projects: ', projects);
+        console.log('tasks: ', tasks);
+        console.log('timelogs: ', timelogs);
+    }, [projects, tasks, timelogs]);
     return <>Vite React Template App</>;
 };
 
