5689c4a27b564374164cb0dcd3844416907060e0
Vincent Sjögren <vincentsjogren@gmail.com>
2022-10-21 14:37:07 +0200

    removed unused import

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-import { isStr, isNum } from 'x-is-type/callbacks';
+import { isStr } from 'x-is-type/callbacks';
 
 export const classNames = (...names: unknown[]) => {
     return names.filter((name) => name && isStr(name)).join(' ');
