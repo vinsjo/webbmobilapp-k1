d57677852cc8fa6ddb3c49ecd75013e5f9e6c08d
Vincent Sjögren <vincentsjogren@gmail.com>
2022-10-21 14:55:33 +0200

    updated theme

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,19 @@
-import { MantineProvider, Box } from '@mantine/core';
+import { useMemo } from 'react';
+import { MantineProvider, Box, type MantineTheme } from '@mantine/core';
+import { useColorScheme } from '@mantine/hooks';
 
 const Layout = (props: React.PropsWithChildren) => {
+    const colorScheme = useColorScheme();
+    const theme = useMemo<Partial<MantineTheme>>(() => {
+        return {
+            colorScheme,
+            focusRing: 'never',
+            primaryColor: 'gray',
+        };
+    }, [colorScheme]);
+
     return (
-        <MantineProvider withGlobalStyles withNormalizeCSS>
+        <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
             <Box>{props.children}</Box>
         </MantineProvider>
     );
