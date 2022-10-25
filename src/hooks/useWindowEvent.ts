fb785809d307c6be8b0746b11a7405f0c995161a
Vincent Sjögren <vincentsjogren@gmail.com>
2022-10-25 19:04:44 +0200

    updated useWindowEvent hook

--- src/hooks/useWindowEvent.ts
+++ src/hooks/useWindowEvent.ts
@@ -1,22 +1,19 @@
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 
 /**
  * Use window event as a hook
  *
- * @param type type of event
+ * @param {keyof WindowEventMap} type type of event
  * @param listener callback to be executed when event is fired,
  * preferably a memoized function to prevent unnecessary re-renders
  */
 export default function useWindowEvent<T extends keyof WindowEventMap>(
     type: T,
-    listener: (this: Window, ev: WindowEventMap[T]) => unknown,
-    options?: boolean | EventListenerOptions
+    listener: (this: Window, ev: WindowEventMap[T]) => unknown
 ) {
-    const optionsRef = useRef(options);
     useEffect(() => {
         if (typeof listener !== 'function') return;
-        const options = optionsRef.current;
-        window.addEventListener(type, listener, options);
-        return () => window.removeEventListener(type, listener, options);
+        window.addEventListener(type, listener);
+        return () => window.removeEventListener(type, listener);
     }, [type, listener]);
 }
